c9864ecc34243858d898f78215685661f8034532
gidigidiolu <iy@gidigidiolu.com>
2020-08-26 22:23:38 +0100

    add ex. 5.14

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,27 +1,47 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 import { prettyDOM } from '@testing-library/dom'
 
-test('renders only blog title and author by default', () => {
-  const blog = {
-    title: 'sample title',
-    author: 'John Doe',
-    url: 'http://www.example.com',
-    likes: 10,
-    user: {
-      username: 'fooBar',
-    },
-  }
+describe('<Blog />', () => {
+  let component
 
-  const component = render(<Blog blog={blog} user={blog.user} />)
-  const titleAndAuthorDiv = component.container.querySelector('.titleAndAuthor')
-  const detailsDiv = component.container.querySelector('.details')
+  beforeEach(() => {
+    const blog = {
+      title: 'sample title',
+      author: 'John Doe',
+      url: 'http://www.example.com',
+      likes: 10,
+      user: {
+        username: 'fooBar',
+      },
+    }
 
-  // component.debug()
-  expect(titleAndAuthorDiv).toBeVisible()
-  expect(detailsDiv).not.toBeVisible()
+    component = render(<Blog blog={blog} user={blog.user} />)
+  })
 
-  console.log(prettyDOM(titleAndAuthorDiv))
+  test('renders only blog title and author by default', () => {
+    const titleAndAuthorDiv = component.container.querySelector(
+      '.titleAndAuthor'
+    )
+    const detailsDiv = component.container.querySelector('.details')
+
+    expect(titleAndAuthorDiv).toBeVisible()
+    expect(detailsDiv).not.toBeVisible()
+  })
+
+  test('also renders details when view button is clicked', () => {
+    const viewButton = component.getByText('view')
+    fireEvent.click(viewButton)
+
+    const titleAndAuthorDiv = component.container.querySelector(
+      '.titleAndAuthor'
+    )
+    const detailsDiv = component.container.querySelector('.details')
+
+    // component.debug()
+    expect(titleAndAuthorDiv).toBeVisible()
+    expect(detailsDiv).toBeVisible()
+  })
 })
